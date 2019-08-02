3129941311e974b0dd9ff380d2f4b5373b40eca1
peoray <peoray@yahoo.com>
2019-08-02 13:00:02 +0100

    add rows

--- components.js
+++ components.js
@@ -1,11 +1,11 @@
 Vue.component('appNavbar', {
-    props: ['icon', 'text'],
-    computed: {
-        iconClass() {
-            return `fas fa-${this.icon}`
-        }
-    },
-    template: `
+  props: ['icon', 'text'],
+  computed: {
+    iconClass() {
+      return `fas fa-${this.icon}`
+    }
+  },
+  template: `
     <nav class="hl_navbar shrink" id="navbar">
       <div class="hl_navbar--inner">
         <a href="./" class="hl_navbar--logo">
@@ -154,13 +154,13 @@ Vue.component('appNavbar', {
 
 
 Vue.component('appHeader', {
-    props: ['icon', 'text'],
-    computed: {
-        iconClass() {
-            return `fas fa-${this.icon}`
-        }
-    },
-    template: `
+  props: ['icon', 'text'],
+  computed: {
+    iconClass() {
+      return `fas fa-${this.icon}`
+    }
+  },
+  template: `
  
 <header class="hl_header nav-shrink ">
 <div class="container-fluid">
@@ -283,6 +283,71 @@ Vue.component('appHeader', {
 
 
 
+Vue.component('appPane', {
+
+  template: `
+      <div class="hl_page-creator--column">
+      <div class="hl_page-creator--element">
+        <div class="hl_page-creator--actions">
+          <div class="more-actions">
+            <span data-tooltip="tooltip" data-placement="top" title="Move"><i
+                class="fas fa-arrows-alt"></i></span>
+            <span data-tooltip="tooltip" data-placement="top" title="Clone"><i
+                class="far fa-eye"></i></span>
+            <span data-tooltip="tooltip" data-placement="top" title="Save"><i
+                class="far fa-copy"></i></span>
+            <span data-tooltip="tooltip" data-placement="top" title="Delete"><i
+                class="far fa-trash-alt"></i></span>
+          </div>
+        </div>
+        <span class="add-new-element" data-tooltip="tooltip" data-placement="bottom"
+          title="Add New Element"><i class="icon icon-plus"></i></span>
+        <div class="element-container">
+          <h1>Heading Text Goes Here</h1>
+        </div>
+      </div>
+      <div class="hl_page-creator--element">
+        <div class="hl_page-creator--actions">
+          <div class="more-actions">
+            <span data-tooltip="tooltip" data-placement="top" title="Move"><i
+                class="fas fa-arrows-alt"></i></span>
+            <span data-tooltip="tooltip" data-placement="top" title="Clone"><i
+                class="far fa-eye"></i></span>
+            <span data-tooltip="tooltip" data-placement="top" title="Save"><i
+                class="far fa-copy"></i></span>
+            <span data-tooltip="tooltip" data-placement="top" title="Delete"><i
+                class="far fa-trash-alt"></i></span>
+          </div>
+        </div>
+        <span class="add-new-element" data-tooltip="tooltip" data-placement="bottom"
+          title="Add New Element"><i class="icon icon-plus"></i></span>
+        <div class="element-container" style="max-width: 700px; margin: 0 auto;">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem architecto quas mollitia
+            voluptates iure consequuntur totam numquam quasi quaerat, iste, libero asperiores optio
+            praesentium autem culpa doloremque laudantium in deleniti,
+            corrupti eaque illo nam quae. Vel nesciunt, libero ipsa similique ipsum ducimus fuga magni.
+            Voluptatum iste pariatur cum doloremque est.</p>
+        </div>
+      </div>
+    </div>
+  `
+})
+
+
 new Vue({
-    el: '#app1'
+  el: '#app1',
+  data() {
+    return {
+      rows: []
+    }
+  },
+  methods: {
+    addRow() {
+      this.rows.push({
+        // id: this.rows.length,
+        elements: []
+      })
+      console.log('Hello world')
+    }
+  }
 })

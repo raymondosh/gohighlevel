498c8d578cc3e63f0ee511f2285e70dbbe555069
peoray <peoray@yahoo.com>
2019-08-02 13:50:31 +0100

    make edit

--- components.js
+++ components.js
@@ -287,7 +287,7 @@ Vue.component('appPane', {
 
   template: `
   <section class="hl_page-creator--section"
-  style="background-color: #f4f4f4; padding: 80px 0; text-align: center;" >
+  style="background-color: #f4f4f4; padding: 80px 0; text-align: center;">
   <div class="hl_page-creator--actions">
     <div class="move-actions">
       <span data-tooltip="tooltip" data-placement="right" title="Up"><i
@@ -323,50 +323,8 @@ Vue.component('appPane', {
     </div>
     <span class="add-new-row" data-tooltip="tooltip" data-placement="bottom" title="Add New Row"><i
         class="icon icon-plus"></i></span>
-      <div class="hl_page-creator--column">
-      <div class="hl_page-creator--element">
-        <div class="hl_page-creator--actions">
-          <div class="more-actions">
-            <span data-tooltip="tooltip" data-placement="top" title="Move"><i
-                class="fas fa-arrows-alt"></i></span>
-            <span data-tooltip="tooltip" data-placement="top" title="Clone"><i
-                class="far fa-eye"></i></span>
-            <span data-tooltip="tooltip" data-placement="top" title="Save"><i
-                class="far fa-copy"></i></span>
-            <span data-tooltip="tooltip" data-placement="top" title="Delete"><i
-                class="far fa-trash-alt"></i></span>
-          </div>
-        </div>
-        <span class="add-new-element" data-tooltip="tooltip" data-placement="bottom"
-          title="Add New Element"><i class="icon icon-plus"></i></span>
-        <div class="element-container">
-          <h1>Heading Text Goes Here</h1>
-        </div>
-      </div>
-      <div class="hl_page-creator--element">
-        <div class="hl_page-creator--actions">
-          <div class="more-actions">
-            <span data-tooltip="tooltip" data-placement="top" title="Move"><i
-                class="fas fa-arrows-alt"></i></span>
-            <span data-tooltip="tooltip" data-placement="top" title="Clone"><i
-                class="far fa-eye"></i></span>
-            <span data-tooltip="tooltip" data-placement="top" title="Save"><i
-                class="far fa-copy"></i></span>
-            <span data-tooltip="tooltip" data-placement="top" title="Delete"><i
-                class="far fa-trash-alt"></i></span>
-          </div>
-        </div>
-        <span class="add-new-element" data-tooltip="tooltip" data-placement="bottom"
-          title="Add New Element"><i class="icon icon-plus"></i></span>
-        <div class="element-container" style="max-width: 700px; margin: 0 auto;">
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem architecto quas mollitia
-            voluptates iure consequuntur totam numquam quasi quaerat, iste, libero asperiores optio
-            praesentium autem culpa doloremque laudantium in deleniti,
-            corrupti eaque illo nam quae. Vel nesciunt, libero ipsa similique ipsum ducimus fuga magni.
-            Voluptatum iste pariatur cum doloremque est.</p>
-        </div>
-      </div>
-    </div>
+
+      <slot></slot>
     </div>
     </section>
   `
